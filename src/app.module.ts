616a2e625d0c9b11f2b2ad05bd72f4bca74e726a
DamiiPeralta <damii-peralta@hotmail.com>
2024-12-19 15:34:58 -0300

    FT:paypal integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PayPalModule } from './suscription/paypal/paypal.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PayPalModule, PrismaModule],
 })
 export class AppModule {}
---
+++ src/suscription/paypal/services/transaction.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma/prisma.service';
+
+@Injectable()
+export class TransactionService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async createTransaction(data: {
+    orderId: string;
+    status: string;
+    amount: number;
+    currency: string;
+    userId: string;
+  }) {
+    return this.prisma.transaction.create({ data });
+  }
+
+  async getTransactionsByUser(userId: string) {
+    return this.prisma.transaction.findMany({
+      where: { userId },
+    });
+  }
+}
