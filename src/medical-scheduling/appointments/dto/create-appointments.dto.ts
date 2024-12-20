7a47780a763724b9e99009261955ea7c0ada1d0d
Jeison <Jeisonlopezarroyave@gmail.com>
2024-12-19 21:27:12 -0500

    refactor: update models, DTOs, and interfaces based on Figma design
    - Modified model fields in schema.prisma
    - Updated DTOs and interfaces to match the Figma design

--- src/medical-scheduling/appointments/dto/create-appointments.dto.ts
+++ src/medical-scheduling/appointments/dto/create-appointments.dto.ts
@@ -1,9 +1,9 @@
 export class CreateAppointmentDto {
+  consultation_reason: string;
   start: Date;
   end: Date;
   patient_id: string;
   physician_id: string;
-  status: 'Atendida' | 'Cancelada' | 'Pendiente';
-  cancelation_reason?: string;
+  status?: 'Atendida' | 'Cancelada' | 'Pendiente';
   tenant_id: string;
 }
--- src/medical-scheduling/medical-events/dto/create-medical-event.dto.ts
+++ src/medical-scheduling/medical-events/dto/create-medical-event.dto.ts
@@ -1,11 +1,12 @@
 export class CreateMedicalEventDto {
   appointment_id: string;
-  consultation_reason: string;
+  patient_id: string;
+  physician_id: string;
   physician_comments?: string;
   main_diagnostic_cie: string;
+  consultation_reason: string;
   evolution?: string;
   procedure?: string;
   treatment?: string;
-  comments?: string;
   tenant_id: string;
 }
