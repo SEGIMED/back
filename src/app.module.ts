ee25ae77b9b521b60be4f4637013a96e69ab651f
Jeison <Jeisonlopezarroyave@gmail.com>
2024-12-26 18:48:13 -0500

    feat: add PrismaService to the AppModule providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppointmentsModule } from './medical-scheduling/appointments/appointments.module';
 import { MedicalEventsModule } from './medical-scheduling/medical-events/medical-events.module';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   imports: [AppointmentsModule, MedicalEventsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
