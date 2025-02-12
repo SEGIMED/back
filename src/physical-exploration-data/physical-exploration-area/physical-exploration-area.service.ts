541de11869bdb37d6f9f6d6c8d3f334796b3e72d
Jeison <Jeisonlopezarroyave@gmail.com>
2025-02-12 16:39:25 -0500

    Add validation to createPhysicalExplorationArea; check for existing areas and improve error handling

--- src/physical-exploration-data/physical-exploration-area/physical-exploration-area.service.ts
+++ src/physical-exploration-data/physical-exploration-area/physical-exploration-area.service.ts
@@ -8,13 +8,29 @@ export class PhysicalExplorationAreaService {
 
   async createPhysicalExplorationArea(data: CreatePhysicalExplorationAreaDto) {
     try {
+      const existingArea =
+        await this.prisma.physical_exploration_area.findFirst({
+          where: {
+            OR: [
+              { name_on_library: data.name_on_library },
+              { name: data.name },
+            ],
+          },
+        });
+
+      if (existingArea) {
+        throw new HttpException(
+          'Physical exploration area with the same name or name_on_library already exist',
+          400,
+        );
+      }
       return await this.prisma.physical_exploration_area.create({
         data,
       });
     } catch (error) {
       throw new HttpException(
-        'Error creating physical exploration area',
-        error.message,
+        error.message || 'Error creating physical exploration area',
+        error.status || 500,
       );
     }
   }
